de80bda95b1034efdd8fe7723e31fb3e27056713
Oliver Lade <Oliver.Lade@anz.com>
2016-08-21 21:16:08 +1000

    Greatly expand implementation of LinkedList, revert to java.util interfaces, add Guava collection testing

--- src/main/java/net/piemaster/jad/data/collections/lists/LinkedList.java
+++ src/main/java/net/piemaster/jad/data/collections/lists/LinkedList.java
@@ -3,8 +3,14 @@ package net.piemaster.jad.data.collections.lists;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 /**
- * A singly-linked list.
+ * A naive singly-linked list. Strives for the simplest implementation, not the most efficient.
  * <p/>
  * New items are appended to the tail of the list.
  *
@@ -51,50 +57,95 @@ public class LinkedList<T> implements List<T> {
     // INSERTION
 
     @Override
-    public void add(T item) {
+    public boolean add(T item) {
+        // Null is allowed.
         Node node = new Node(item);
         if (head == null) {
             head = node;
         } else {
             getLastNode().setNext(node);
         }
+        return true;
     }
 
     @Override
-    public void insert(T item, int index) {
-        int i = 0;
-        Node previous = null;
-        Node current = head;
-
+    public void add(int index, T element) {
         if (index < 0) {
-            throw new IndexOutOfBoundsException(
-                    String.format("Index %d must be non-negative", index));
+            throw new IndexOutOfBoundsException("Cannot add outside list bounds");
+        }
+        java.util.ListIterator<T> iter = listIterator();
+        while (iter.nextIndex() < index) {
+            if (!iter.hasNext()) {
+                throw new IndexOutOfBoundsException("Cannot add outside list bounds");
+            }
+            iter.next();
+        }
+        iter.add(element);
+    }
+
+    // NAIVE
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        java.util.ListIterator<T> iter = listIterator();
+        while (iter.hasNext()) {
+            iter.next();
+        }
+        boolean changed = false;
+        for (T item : c) {
+            iter.add(item);
+            changed = true;
+        }
+        return changed;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("Cannot set outside list bounds");
+        }
+        if (c == null) {
+            throw new NullPointerException("Must provide collection to add");
+        } else if (c.isEmpty()) {
+            return false;
         }
 
-        // Update the "previous" pointer to the node before the insert.
-        while (i < index) {
-            if (current == null) {
-                throw new IndexOutOfBoundsException(
-                        String.format("Insert index %d > size %d", index, i));
+        java.util.ListIterator<T> iter = listIterator();
+        while (iter.nextIndex() < index) {
+            if (!iter.hasNext()) {
+                throw new IndexOutOfBoundsException("Cannot set outside list bounds");
             }
-            previous = current;
+            iter.next();
+        }
+
+        for (T item : c) {
+            iter.add(item);
+        }
+        return true;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("Cannot set outside list bounds");
+        }
+        int i = 0;
+        Node current = head;
+        while (current != null && i < index) {
             current = current.getNext();
             i++;
         }
-
-        Node node = new Node(item);
-        node.setNext(current);
-        if (previous != null) {
-            previous.setNext(node);
-        } else {
-            head = node;
+        if (current == null) {
+            throw new IndexOutOfBoundsException("Cannot set outside list bounds");
         }
+        T oldValue = current.getValue();
+        current.setValue(element);
+        return oldValue;
     }
 
     // DELETION
 
     @Override
-    public boolean remove(T item) {
+    public boolean remove(Object item) {
         Node previous = null;
         Node current = head;
         while (current != null) {
@@ -115,76 +166,102 @@ public class LinkedList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        if (index < 0) {
-            throw new IndexOutOfBoundsException("Index must be non-negative");
-        }
-        if (head == null) {
-            throw new IndexOutOfBoundsException("Index exceeds list size of 0");
+        if (index < 0 || head == null) {
+            throw new IndexOutOfBoundsException("Cannot remove outside list bounds");
         }
 
-        int i = 0;
-        Node current = head;
-        Node previous = null;
-
-        while (i < index) {
-            previous = current;
-            current = current.getNext();
-            if (current == null) {
-                throw new IndexOutOfBoundsException("Index exceeds list size of " + i);
+        java.util.ListIterator<T> iter = listIterator();
+        T value = null;
+        while (iter.previousIndex() < index) {
+            if (!iter.hasNext()) {
+                throw new IndexOutOfBoundsException("Cannot remove outside list bounds");
             }
-            i++;
+            value = iter.next();
         }
+        iter.remove();
+        return value;
+    }
 
-        if (previous != null) {
-            previous.setNext(current.getNext());
-        } else {
-            head = current.getNext();
+    // NAIVE
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean changed = false;
+        for (Object item : c) {
+            changed |= remove(item);
         }
-        return current.getValue();
+        return changed;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        java.util.ListIterator<T> iter = listIterator();
+        boolean changed = false;
+        while (iter.hasNext()) {
+            if (!c.contains(iter.next())) {
+                iter.remove();
+                changed = true;
+            }
+        }
+        return changed;
+    }
+
+    @Override
+    public void clear() {
+        head = null;
     }
 
     // SEARCH
 
     @Override
-    public boolean contains(T item) {
+    public boolean contains(Object item) {
         return indexOf(item) != -1;
     }
 
-    public T get(int index) {
-        if (index < 0) {
-            throw new IndexOutOfBoundsException("Index must be non-negative");
-        }
-        if (head == null) {
-            throw new IndexOutOfBoundsException("Index exceeds list size of 0");
-        }
-
-        int i = 0;
-        Node current = head;
-
-        while (i < index) {
-            current = current.getNext();
-            if (current == null) {
-                throw new IndexOutOfBoundsException("Index exceeds list size of " + i);
+    // NAIVE
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object item : c) {
+            if (!contains(item)) {
+                return false;
             }
-            i++;
         }
-        return current.getValue();
+        return true;
+    }
+
+    public T get(int index) {
+        if (index < 0 || head == null) {
+            throw new IndexOutOfBoundsException("Cannot get outside list bounds");
+        }
+        try {
+            return listIterator(index).next();
+        } catch (Exception e) {
+            throw new IndexOutOfBoundsException("Cannot get outside list bounds");
+        }
     }
 
     @Override
-    public int indexOf(T item) {
-        int i = 0;
-        Node current = head;
-
-        while (current != null) {
-            if (item == null && current.getValue() == null ||
-                    item != null && item.equals(current.getValue())) {
-                return i;
+    public int indexOf(Object item) {
+        java.util.ListIterator<T> iter = listIterator();
+        while (iter.hasNext()) {
+            T value = iter.next();
+            if (item == null && value == null || item != null && item.equals(value)) {
+                return iter.previousIndex();
             }
-            current = current.getNext();
-            i++;
         }
         return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        java.util.ListIterator<T> iter = listIterator();
+        int index = -1;
+        while (iter.hasNext()) {
+            T value = iter.next();
+            if (o == null && value == null || o != null && o.equals(value)) {
+                index = iter.previousIndex();
+            }
+        }
+        return index;
     }
 
     Node getLastNode() {
@@ -199,18 +276,223 @@ public class LinkedList<T> implements List<T> {
         return current;
     }
 
-    // PROPERTIES
+    // ITERATORS
+
+    @Override
+    public java.util.ListIterator<T> listIterator() {
+        return new ListIterator();
+    }
+
+    @Override
+    public java.util.ListIterator<T> listIterator(int index) {
+        return new ListIterator(index);
+    }
+
+    @Override
+    public java.util.List<T> subList(int fromIndex, int toIndex) {
+        if (fromIndex < 0 || fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException();
+        }
+        java.util.ListIterator<T> iter = listIterator(fromIndex);
+        LinkedList<T> sublist = new LinkedList<T>();
+        while (iter.nextIndex() < toIndex) {
+            if (!iter.hasNext()) {
+                throw new IndexOutOfBoundsException();
+            }
+            sublist.add(iter.next());
+        }
+        return sublist;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
 
     @Override
     public int size() {
-        int count = 0;
-        Node current = head;
-        while (current != null) {
-            current = current.getNext();
-            count++;
+        java.util.ListIterator<T> iter = listIterator();
+        while(iter.hasNext()) {
+            iter.next();
         }
-        return count;
+        return iter.nextIndex();
     }
 
+    @Override
+    public Iterator<T> iterator() {
+        return new ListIterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return toArray(new Object[0]);
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        int size = size();
+        if (a.length < size) {
+            a = (T1[]) Array.newInstance(a.getClass().getComponentType(), size);
+        }
+
+        int i = 0;
+        for (T item : this) {
+            a[i++] = (T1) item;
+        }
+        while (i < a.length) {
+            a[i++] = null;
+        }
+        return a;
+    }
+
+    // OBJECT METHODS
+
+    @Override
+    public boolean equals(Object other) {
+        // Element-wise comparison with other list.
+        if (other instanceof java.util.List) {
+            java.util.ListIterator iter = listIterator();
+            java.util.ListIterator otherIter = ((java.util.List) other).listIterator();
+            while (iter.hasNext() || otherIter.hasNext()) {
+                if (iter.hasNext() != otherIter.hasNext()) {
+                    return false;
+                }
+                Object value = iter.next();
+                Object otherValue = otherIter.next();
+                if (value == null && otherValue != null
+                        || value != null && !value.equals(otherValue)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return super.equals(other);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Iterator<T> iterator = iterator();
+        if (iterator.hasNext()) {
+            sb.append(iterator.next());
+        }
+        while (iterator.hasNext()) {
+            sb.append(", ").append(iterator.next());
+        }
+        return sb.append("]").toString();
+    }
+
+    /**
+     * Iterates over the items in the list with additional list features.
+     */
+    private class ListIterator implements java.util.ListIterator<T> {
+        private int previousIndex = -1;
+        private int nextIndex = 0;
+
+        private Node previous, current = null;
+        private Node next = head;
+
+        public ListIterator() {
+        }
+
+        public ListIterator(int index) {
+            if (index < 0) {
+                throw new IndexOutOfBoundsException("Cannot create iterator outside list bounds");
+            }
+            while (nextIndex < index) {
+                if (!hasNext()) {
+                    throw new IndexOutOfBoundsException("Cannot create iterator outside list bounds");
+                }
+                next();
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public T next() {
+            if (next == null) {
+                throw new NoSuchElementException("No more elements in list");
+            }
+            T value = next.getValue();
+
+            // In case current was removed.
+            if (current != null) {
+                previous = current;
+            }
+            current = next;
+            next = next.getNext();
+            nextIndex++;
+            previousIndex++;
+            return value;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return current != null || previous != null;
+        }
+
+        @Override
+        public T previous() {
+            if (previous == null) {
+                throw new NoSuchElementException("No previous element");
+            }
+            return previous.getValue();
+        }
+
+        @Override
+        public int nextIndex() {
+            return nextIndex;
+        }
+
+        @Override
+        public int previousIndex() {
+            return previousIndex;
+        }
+
+        @Override
+        public void remove() {
+            if (current == null) {
+                throw new IllegalStateException("Cannot remove before next or twice in a row");
+            }
+            if (current == head) {
+                head = next;
+            }
+            current = null;
+            if (previous != null) {
+                previous.setNext(next);
+            }
+            nextIndex--;
+        }
+
+        @Override
+        public void set(T t) {
+            if (current == null) {
+                throw new IllegalStateException("Cannot set after remove");
+            }
+            current.setValue(t);
+        }
+
+        @Override
+        public void add(T t) {
+            Node newNode = new Node(t);
+            if (current != null) {
+                current.setNext(newNode);
+            }
+            if (next != null) {
+                newNode.setNext(next);
+            }
+            if (next == head) {
+                head = newNode;
+            }
+            previous = current;
+            current = newNode;
+            previousIndex++;
+            nextIndex++;
+        }
+    }
 
 }
--- src/test/groovy/net/piemaster/jad/data/collections/lists/LinkedListTest.groovy
+++ src/test/groovy/net/piemaster/jad/data/collections/lists/LinkedListTest.groovy
@@ -1,26 +1,31 @@
 package net.piemaster.jad.data.collections.lists
 
-class LinkedListTest extends ListTest {
+import com.google.common.collect.testing.ListTestSuiteBuilder
+import com.google.common.collect.testing.TestStringListGenerator
+import com.google.common.collect.testing.features.CollectionFeature
+import com.google.common.collect.testing.features.CollectionSize
+import com.google.common.collect.testing.features.ListFeature
+import junit.framework.Test
 
-    def "get the last node"() {
-        given:
-        instance = createInstanceWithItems(String.class, TEST_STRINGS)
+/**
+ * Performs Guava's List test suite on the LinkedList implementation.
+ */
+class LinkedListTest {
 
-        when:
-        final result = instance.getLastNode()
-
-        then:
-        result.getValue() == TEST_STRINGS[TEST_STRINGS.length - 1]
-    }
-
-    @Override
-    def <T> List<T> createInstance(Class<T> cls) {
-        return new LinkedList<>()
-    }
-
-    @Override
-    def <T> List<T> createInstanceWithItems(Class<T> cls, T... items) {
-        return new LinkedList<>(items)
+    public static Test suite() {
+        return ListTestSuiteBuilder.using(
+                new TestStringListGenerator() {
+                    @Override
+                    protected List<String> create(String[] elements) {
+                        return new LinkedList(elements)
+                    }
+                })
+                .named("LinkedList Tests")
+                .withFeatures(
+                ListFeature.GENERAL_PURPOSE,
+                CollectionFeature.ALLOWS_NULL_VALUES,
+                CollectionSize.ANY)
+                .createTestSuite();
     }
 
 }
